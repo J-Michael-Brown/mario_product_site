0662cc644bf935752864f785979319702c5f0c79
John Michael Brown <jmichaelbrown132737@gmail.com>
2018-12-14 10:43:02 -0800

    add functionality to add new products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,29 @@
+require 'pry'
+
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    render :index
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to  products_path
+    else
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :description) # permit takes a *splat
   end
 end
