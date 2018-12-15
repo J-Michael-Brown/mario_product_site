7f954671774cec3609bfb64709292e9983644510
John Michael Brown <jmichaelbrown132737@gmail.com>
2018-12-14 16:40:05 -0800

    add minot CSS styling.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,16 +4,16 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :description, :presence => true
 
-  def self.reviews(product)
-    reviews = []
-    sql_reviews = PG.connect({:dbname => "marios_specialty_food_products_development"}).exec("SELECT * FROM reviews WHERE product_id = #{product.id};")
-    sql_reviews.each() do |review|
-      title = review.fetch("title")
-      id = review.fetch("id").to_i()
-      description = review.fetch("description")
-      rating = review.fetch("rating").to_i
-      reviews.push(Book.new({:title => title, :id => id, :description => description, :rating => rating}))
+  def rating
+    reviews = self.reviews
+    if reviews.any?
+      average = 0
+      reviews.each do |review|
+        average += review.rating
+      end
+      return (10*average/reviews.length).to_f/10
+    else
+      false
     end
-    reviews
   end
 end
